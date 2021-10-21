8597a8c49d0c23cd7d267a525e9acd9d6884ec69
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-21 18:03:38 +0530

    [Chaitanya] Add : Added fare calculator per ride

---
+++ CabInvoiceGenerator/FareCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    class FareCalculator
+    {
+        const int MINIMUM_FARE = 5, COST_PER_KM = 10, Cost_PER_MINUTE = 1;
+        public double CalculateFare(double distanceTravelledInKm, double timeOfTravelInMinutes)
+        {
+            double fare = (distanceTravelledInKm * COST_PER_KM) + (timeOfTravelInMinutes * Cost_PER_MINUTE);
+            return Math.Max(fare, MINIMUM_FARE);
+        }
+    }
+}
--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -6,7 +6,25 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool isRun = true;
+            while (isRun)
+            {
+                FareCalculator fareCalculator = new FareCalculator();
+                Console.WriteLine("Select and enter UC number to run:\n" +
+                    "1. Calcate Fare per ride");
+                int ucNumber = Convert.ToInt32(Console.ReadLine());
+                switch (ucNumber)
+                {
+                    case 1:
+                        double distanceTravelledInKm = 0.3, timeOfTravelInMinutes = 1;
+                        double fare = fareCalculator.CalculateFare(distanceTravelledInKm, timeOfTravelInMinutes);
+                        Console.WriteLine($"Fare for ride of {distanceTravelledInKm} Km in {timeOfTravelInMinutes} minutes is: Rs.{fare}");
+                        break;
+                    default:
+                        isRun = !isRun;
+                        break;
+                }
+            }
         }
     }
 }
